f387b1cc6616b5635d65a1bf5407716769f07fe9
Saksham <sakshamsingh5001@gmail.com>
2024-06-05 15:07:49 +0530

    Create initial project

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React,{useState,useEffect} from 'react'
+import './Home.css'
+import jsPDF from 'jspdf'
+
+const Home = () => {
+    console.log(jsPDF)
+    return (
+        <div className='home-container'>
+
+        </div>
+    )
+}
+
+export default Home
